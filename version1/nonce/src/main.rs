6432f4b69d0b4d541997d4453c0ff8b4e50df032
peterknight160-dotcom <peter.knight160@gmail>
2026-06-03 16:29:10 +0100

    nonce update2

--- version1/nonce/src/main.rs
+++ version1/nonce/src/main.rs
@@ -1,29 +1,44 @@
-use std::time::{ SystemTime, UNIX_EPOCH};
 use chacha20::ChaCha20Rng;
-use rand_core::{SeedableRng, Rng};
+use rand_core::{Rng, SeedableRng};
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
+
+const HOWMANY: u32 = 1000000;
 
 fn main() {
-    
-    let time1 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros().to_string();
+    let time1 = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_micros()
+        .to_string();
     let bytes = time1.as_bytes();
-    let mut seed :[u8;32]   = [0;32];
+    let mut seed = [0u8; 32];
     for (i, value) in bytes.iter().enumerate() {
         seed[i] = *value;
     }
-      for (i, value) in bytes.iter().enumerate() {
-        seed[31-i] = *value;
+    for (i, value) in bytes.iter().enumerate() {
+        seed[31 - i] = *value;
     }
-    println!( "Seed is {:?}", seed);
+    println!("Seed is {:?}", seed);
 
     //println!("return_val: {:?} ", return_val);
-// let time2 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
-//  println!{"time2 is {} ", time2}
-let mut rng = ChaCha20Rng::from_seed(seed);
-for _ in 0..100 {
+    // let time2 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+    //  println!{"time2 is {} ", time2}
+    let mut rng = ChaCha20Rng::from_seed(seed);
+    let start = Instant::now();
 
-println!(" Random number is {}", rng.next_u64() as f32 / u64::MAX as f32);
-}
-}
+    for _ in 0..HOWMANY/128 {
+        let mut sum = 0u64;
+        for _ in 0..128 {
+            sum += rng.next_u64() <<2;
+           
+        }
+         println!(" Random number is {}", sum);
+    }
 
+    let elapsed = start.elapsed();
+
+    println!("That took {:.2?}", elapsed);
+    println!("That took {:.2?} per random", elapsed / HOWMANY);
+}
 
 //    return_val= SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs().to_string();
