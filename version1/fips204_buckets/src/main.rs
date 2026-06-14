2860b5c007f79640aeabbeab7352dc5e5477ffd0
peterknight160-dotcom <peter.knight160@gmail>
2026-06-14 18:03:16 +0100

    More stats work on the algos

--- version1/fips204_buckets/src/main.rs
+++ version1/fips204_buckets/src/main.rs
@@ -1,38 +1,56 @@
-#[allow(unused_imports)]
+
 use fips204::ml_dsa_44; // Could also be ml_dsa_44 or ml_dsa_44.
 //use fips204::traits::{SerDes, Signer, Verifier};
 use fips204::traits::Signer;
+use useful_stats::*; 
+
+
 use std::time::Instant;
+use std::collections::BTreeMap;
 
 fn check_fips204(message: &[u8], nkeys: u32, nloops: u32) -> Option<bool> {
     #![allow(unused_variables)]
     println!("Message length: {:?}", message.len());
 
+    let mut key_time_hash: BTreeMap<u128, u32> = BTreeMap::new();
+    let mut sign_time_hash: BTreeMap<u128, u32> = BTreeMap::new();
     // Generate key pair and signature
 
-    let Some((pk1, sk)) = ml_dsa_44::try_keygen().ok() else {
-        panic!("At line 7")
-    }; // Generate both public and secret keys
-
-    let mut key_time: u128 = 0;
+  
 
     // Outer loop - number of keys to use
-    for _ in 0..nkeys {
+    for ikeys in 0..nkeys {
         let start_key = Instant::now();
         let Some((pk1, sk)) = ml_dsa_44::try_keygen().ok() else {
             panic!("At line 7");
         };
-        key_time += start_key.elapsed().as_micros();
-    } // Generate both public and secret keys for this loop
-    for _ in 0..nloops {
+        let key_time   = start_key.elapsed().as_micros();
+      
+        *key_time_hash.entry(key_time).or_insert(0) +=1; 
+        
+        for _ in 0..nloops {
+            let start_sign = Instant::now();
         let sig: Option<[u8; 2420]> = sk.try_sign(&message, &[]).ok(); // Use the secret key to generate a message signature
-        //dump.push(sig.unwrap()[0]);
+        let sign_time = start_sign.elapsed().as_micros();
+         *sign_time_hash.entry(sign_time).or_insert(0) +=1; 
     }
 
-    println!(" Key creation time {:?}", (key_time as f64)/(nkeys as f64) );
+
+
+
+    }  
+   
+    println! ( "Stats {} ", stats_from_btree(key_time_hash, "Keys Generation"));
+     println! ( "Stats {} ", stats_from_btree(sign_time_hash, "Message Signing"));
+   
+
+  
+  
+    
 
     Some(true)
 }
+
 
 fn main() {
     // Get iterations from the env vars
@@ -50,8 +68,7 @@ fn main() {
         true => nloops = eloops.unwrap().parse::<u32>().unwrap(),
         false => nloops = 10,
     }
-
-    println!("nkeys is: {}, nlookps is: {}  ", nkeys, nloops);
+   
 
     // Read message from file json.txt
     let message = std::fs::read("json.txt").expect("Unable to read file");
---
+++ version1/useful_stats/src/lib.rs
@@ -0,0 +1,94 @@
+use core::f64;
+use std::fmt::{self, Display, Formatter};
+use std::collections::BTreeMap;
+pub struct  StatsResult {
+    legend: String  ,
+    elements: u32 , // Number of elements found
+    minimum: u128, // Smallest element
+    maximum: u128, // Smallest element
+    mean:  f64 ,
+    std_dev: f64,
+    ninety_pct: u128,
+    ninety_nine_pct: u128
+}
+
+impl Display for StatsResult {
+    fn fmt (&self, f: &mut Formatter ) -> fmt::Result {
+        let _ = writeln! ( f, "For {} , elements = {}  mean = {:.2} std_dev = {:.2}", self.legend, self.elements, self.mean , self.std_dev);
+        writeln!(f, "Minimum = {} ,  maximum = {} , 90th percentile = {}   99th percentile ={} ", self.minimum, self.maximum, self.ninety_pct, self.ninety_nine_pct)
+    }
+}
+
+
+
+pub fn stats_from_btree ( input:BTreeMap<u128, u32>, legend: &str ) -> StatsResult {
+ // Calculate the mean from the hash
+    let mut x: u128 = 0;
+    let mut elements: u32 = 0 ;
+  
+    let mut min: u128 = 1000000;
+    let mut max: u128 = 0;
+    for val in input.keys() {
+        let value= *val;
+        let freq = *input.get(&val).unwrap() ;
+        elements += freq;
+   
+        x +=  value * freq as u128; 
+        match value > max {
+            true => max = value, 
+            false => () ,
+        }
+        match value < min  {
+            true => min = value, 
+            false => () ,
+        }
+    }
+    let meanloop = (  x as f64)/( elements as f64);
+ 
+  
+    // Calculate std deviation, 90 & 99 Percentiles
+    let mut sumsquares:f64 = 0.0;
+    let ninety = (elements *  90 ) /100 ;
+    let ninetynine = (elements *  99 ) /100;
+    let mut elements_so_far : u32 = 0 ;
+    let mut  ninety_pct = 0 ;
+    let mut ninetynine_pct = 0 ;
+
+    for val in input.keys(){
+        let value= *val as f64;
+        let freq = *input.get(&val).unwrap() ;
+        sumsquares += (value - meanloop)*( value  - meanloop) * freq as f64  ;
+        elements_so_far += freq ;
+        if elements_so_far >=  ninety && ninety_pct == 0 {
+            ninety_pct= *val
+            
+            ;
+
+        }
+        if elements_so_far >=  ninetynine && ninetynine_pct == 0 {
+            ninetynine_pct= *val;
+            
+        }
+    }    
+  
+    let std_dev = (sumsquares / elements as f64).sqrt();    
+  
+
+    
+
+    return StatsResult {
+        legend: legend.to_string(),
+        elements: elements,
+        minimum: min,
+        maximum: max,
+        mean: meanloop,
+        std_dev: std_dev,
+        ninety_pct: ninety_pct , 
+        ninety_nine_pct: ninetynine_pct 
+    }
+    
+
+
+}
+
+
