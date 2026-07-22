444a75f5a7af77dadb0bc85aee8415e75d4b0a1c
peterknight160-dotcom <peter.knight160@gmail>
2026-07-22 16:33:10 +0100

    Lattice Kyber Fix

--- evals/lattice_keyber_combined_test/src/main.rs
+++ evals/lattice_keyber_combined_test/src/main.rs
@@ -24,8 +24,8 @@ fn check_fips203(nloops: u32) {
          for _ in 0..nloops/10 {
        
         let kp = MlKemKeyPair::generate(ML_KEM_512).unwrap();
-        let (ct, ss_sender) = kyber::safe_encaps(ML_KEM_512, kp.public_key()).unwrap();
-        let ss_receiver = kp.decaps(&ct).unwrap();
+        let (ct, _ss_sender) = kyber::safe_encaps(ML_KEM_512, kp.public_key()).unwrap();
+        let _ss_receiver = kp.decaps(&ct).unwrap();
        
 
       
@@ -34,19 +34,19 @@ fn check_fips203(nloops: u32) {
     for _ in 0..nloops {
         let start_key = Instant::now();
         let kp = MlKemKeyPair::generate(ML_KEM_512).unwrap();
-        let (ct, ss_sender) = kyber::safe_encaps(ML_KEM_512, kp.public_key()).unwrap();
-        let ss_receiver = kp.decaps(&ct).unwrap();
+        let (ct, _ss_sender) = kyber::safe_encaps(ML_KEM_512, kp.public_key()).unwrap();
+        let _ss_receiver = kp.decaps(&ct).unwrap();
       
 
         let key_time = start_key.elapsed().as_micros();
         *key_time_hash.entry(key_time).or_insert(0) += 1;
     }
 
-    let stats512 = stats_from_btree(&key_time_hash, "MLat_KEM_512 Keys Swap");
+    let stats512 = stats_from_btree(&key_time_hash, "Lattice_Kyber_512 Keys Swap");
     // Get mean + 2 std devs
     let twosigma = stats512.mean * 1.25;
 
-    let _ = draw_histogram_from_btree(&key_time_hash, "MLat_KEM_512_Keys_Swap", twosigma);
+    let _ = draw_histogram_from_btree(&key_time_hash, "Lattice_Kyber_512_Keys_Swap", twosigma);
     println!("Stats {} ", stats512);
 
     // 768
@@ -57,19 +57,19 @@ fn check_fips203(nloops: u32) {
     for _ in 0..nloops {
         let start_key = Instant::now();
         let kp = MlKemKeyPair::generate(ML_KEM_768).unwrap();
-        let (ct, ss_sender) = kyber::safe_encaps(ML_KEM_768, kp.public_key()).unwrap();
-        let ss_receiver = kp.decaps(&ct).unwrap();
+        let (ct, _ss_sender) = kyber::safe_encaps(ML_KEM_768, kp.public_key()).unwrap();
+        let _ss_receiver = kp.decaps(&ct).unwrap();
        
 
         let key_time = start_key.elapsed().as_micros();
         *key_time_hash.entry(key_time).or_insert(0) += 1;
     }
 
-    let stats768 = stats_from_btree(&key_time_hash, "MLat_KEM_768 Keys Swap");
+    let stats768 = stats_from_btree(&key_time_hash, "Lattice_Kyber_768 Keys Swap");
     // Get mean + 2 std devs
     let twosigma = stats768.mean * 1.25;
 
-    let _ = draw_histogram_from_btree(&key_time_hash, "MLat_KEM_768_Keys_Swap", twosigma);
+    let _ = draw_histogram_from_btree(&key_time_hash, "Lattice_Kyber_768_Keys_Swap", twosigma);
     println!("Stats {} ", stats768);
 
     //1024
@@ -79,18 +79,18 @@ fn check_fips203(nloops: u32) {
     for _ in 0..nloops {
         let start_key = Instant::now();
         let kp = MlKemKeyPair::generate(ML_KEM_1024).unwrap();
-        let (ct, ss_sender) = kyber::safe_encaps(ML_KEM_1024, kp.public_key()).unwrap();
-        let ss_receiver = kp.decaps(&ct).unwrap();
+        let (ct, _ss_sender) = kyber::safe_encaps(ML_KEM_1024, kp.public_key()).unwrap();
+        let _ss_receiver = kp.decaps(&ct).unwrap();
         
         let key_time = start_key.elapsed().as_micros();
         *key_time_hash.entry(key_time).or_insert(0) += 1;
     }
 
-    let stats1024 = stats_from_btree(&key_time_hash, "MLat_KEM_1024 Keys Swap");
+    let stats1024 = stats_from_btree(&key_time_hash, "Lattice_Kyber_1024 Keys Swap");
     // Get mean + 2 std devs
     let twosigma = stats1024.mean * 1.25;
 
-    let _ = draw_histogram_from_btree(&key_time_hash, "MLat_KEM_1024_Keys_Swap", twosigma);
+    let _ = draw_histogram_from_btree(&key_time_hash, "Lattice_Kyber_1024_Keys_Swap", twosigma);
     println!("Stats {} ", stats1024);
 }
 
